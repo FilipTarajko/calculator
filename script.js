ed652ee5dea3555199a8402769eebf91555f5d6e
Filip Tarajko <filiptarajko@gmail.com>
2021-03-30 12:15:19 +0200

    add button grid

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log('script works!');
 
+const buttonContainer = document.querySelector('#buttonContainer');
+
 add = (a,b) => a+b;
 substract = (a,b) => a-b;
 multiply = (a,b) => a*b;
@@ -19,3 +21,20 @@ function operate(a, operator, b){
         return divide(a,b);
     }
 }
+
+function drawButtons()
+{
+    for(let i=0; i<4; i++)
+    {
+        for(let j=0; j<4; j++)
+        {
+            let newButton = document.createElement('button');
+            newButton.classList.add('button');
+            newButton.style.height = '100px';
+            newButton.style.width = '100px';
+            buttonContainer.appendChild(newButton);
+        }
+    }
+}
+
+drawButtons();
