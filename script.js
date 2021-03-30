10894daabc6d07a70610e554dee5092bc6aa88ed
Filip Tarajko <filiptarajko@gmail.com>
2021-03-30 12:06:14 +0200

    add styles and logo

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log('script works!');
+
+add = (a,b) => a+b;
+substract = (a,b) => a-b;
+multiply = (a,b) => a*b;
+divide = (a,b) => a/b;
+
+function operate(a, operator, b){
+    if(operator == '+'){
+        return add(a,b);
+    }
+    if(operator == '-'){
+        return substract(a,b);
+    }
+    if(operator == '*'){
+        return multiply(a,b);
+    }
+    if(operator == '/'){
+        return divide(a,b);
+    }
+}
